9f8b8dd6c1f78f924fa566af97d6a0ff71275ab2
yonatan zaken <yonatan.zaken@gmail.com>
2019-11-18 19:37:35 +0200

    WS7 files

---
+++ c/ws7/exws7.c
@@ -0,0 +1,7 @@
+void foo()
+{
+    char str1[20];
+    char *str2 = malloc(sizeof(*str2) * 20);
+    char *str3 = "whatever";
+    char str4[] = "whatever";
+}
---
+++ c/ws7/ws7.c
@@ -0,0 +1,11 @@
+static int x1 = 3;
+static int x2 = 0;
+static int x3;
+int x4 = 10;
+int x5 = 0;
+int x6;
+
+    
+ 
+
+
