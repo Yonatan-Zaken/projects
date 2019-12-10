97df166ffea7296de1799c2d3a087c822e73a3b7
yonatan zaken <yonatan.zaken@gmail.com>
2019-12-10 20:30:21 +0200

    DS8 files

---
+++ ds/sortedlinkedlist/sorted_list.c
@@ -0,0 +1,188 @@
+/*********************************/
+/*   			             	 */
+/*   Implementing file DS 8      */
+/*   Yonatan Zaken		         */
+/*   Last Updated 10/12/19       */
+/*   Reviewed by:            */   
+/*			                   	 */
+/*********************************/
+
+#include <stddef.h> /* size_t */
+#include <assert.h> /* assert */
+#include <stdlib.h> /* malloc */
+
+#include "sortedlist.h" /* sorted linked list functions */
+#include "dllist.h" /* doubley linked list functions */
+
+#define FREE(ptr) free(ptr); ptr = NULL;
+
+#define UNUSED(x) (void)(x)
+
+struct SLL
+{
+    dll_t *list;
+    is_before func;
+    void *param;
+};
+
+sll_t *SortLLCreate(is_before func, void *param)
+{
+    sll_t *new_sll = NULL;
+    
+    assert(NULL != func);
+    
+    new_sll = (sll_t *)malloc(sizeof(sll_t));
+    if (NULL == new_sll)
+    {
+        return NULL;
+    }
+    
+    new_sll->list = DLLCreate();
+    new_sll->func = func;
+    new_sll->param = param;
+    
+    return new_sll;
+}
+
+void SortLLDestroy(sll_t *sll)
+{
+    assert(NULL != sll);    
+    
+    DLLDestroy(sll->list);
+    
+    FREE(sll);
+}
+
+sll_iterator_t SortLLInsert(sll_t *sll, void *data)
+{
+    sll_iterator_t i;
+    sll_iterator_t end;
+    
+    assert(NULL != sll);
+    assert(NULL != data);
+    
+    end = SLLEnd(sll);
+     
+    for (i = SLLBegin(sll); 1 != SLLIsSameIter(i, end); i = SLLNext(i))
+    {
+        if (1 == sll->func(SLLGetData(i), data, sll->param))
+        {
+            break;
+        }
+    }
+    
+    i.current = DLLInsert(sll->list, i.current, data);
+    return i;
+}
+
+sll_iterator_t SortLLRemove(sll_iterator_t it)
+{
+    it.current = DLLRemove(it.current);
+    return (it);     
+}
+
+int SLLIsEmpty(const sll_t *sll)
+{   
+    assert(NULL != sll);
+    
+    return DLLIsEmpty(sll->list);
+}
+
+size_t SLLSize(const sll_t *sll)
+{
+    assert(NULL != sll);
+    
+    return DLLSize(sll->list);
+}
+
+sll_iterator_t SLLBegin(sll_t *sll)
+{
+    sll_iterator_t sll_it;
+    
+    assert(NULL != sll);
+    
+    sll_it.current = DLLBegin(sll->list);
+    
+    return sll_it;
+}
+
+sll_iterator_t SLLEnd(sll_t *sll)
+{
+    sll_iterator_t sll_it;
+    
+    assert(NULL != sll);
+    
+    sll_it.current = DLLEnd(sll->list);
+    
+    return sll_it;
+}
+
+sll_iterator_t SLLNext(sll_iterator_t it)
+{
+    it.current = DLLGetNext(it.current);
+    return it;
+}
+
+sll_iterator_t SLLPrev(sll_iterator_t it)
+{
+    it.current = DLLGetPrev(it.current);
+    return it;    
+}
+
+void *SLLGetData(sll_iterator_t it)
+{
+    return DLLGetData(it.current);
+}
+
+int SLLIsSameIter(const sll_iterator_t it1, const sll_iterator_t it2)
+{
+    return (DLLIsSameIter(it1.current, it2.current));
+}
+
+void* SLLPopBack(sll_t *sll)
+{
+    
+}
+
+void* SLLPopFront(sll_t *sll)
+{
+
+
+}
+
+int SLLForEach(sll_iterator_t start, sll_iterator_t end, action_func_ptr a_ptr, void *ap)
+{
+
+
+}
+
+sll_iterator_t SLLFind(const sll_t *sll, const void *data, sll_iterator_t start, sll_iterator_t end)
+{
+    sll_iterator_t it;
+    
+    assert(NULL != sll);
+    
+    it.current = DLLFind(start.current, end.current, (match_func_ptr)sll->func, 
+                                                                  (void*)data);
+    
+    return it;                                                
+}
+
+sll_iterator_t SLLFindBy(const sll_t *sll, sll_iterator_t start , sll_iterator_t end, match_func_ptr m_ptr, const void *data)
+{
+    sll_iterator_t it;
+    
+    UNUSED(sll);
+    
+    assert(NULL != sll);
+    
+    it.current = DLLFind(start.current, end.current, m_ptr, (void*)data);
+   
+    return it;
+}
+
+void SLLMerge(sll_t *dest, sll_t *src)
+{
+
+}
+
---
+++ ds/sortedlinkedlist/sorted_list_test.c
@@ -0,0 +1,109 @@
+/*********************************/
+/*   			             	 */
+/*   Main file DS 9              */
+/*   Yonatan Zaken		         */
+/*   Last Updated 10/12/19       */
+/*   Reviewed by:            */   
+/*			                   	 */
+/*********************************/
+
+#include <stdio.h> /* printf */
+
+#include "sortedlist.h" /* sorted linked list functions */
+#include "dllist.h" /* doubley linked list functions */
+
+#define NORMAL "\033[0m"
+#define RED "\033[;031m"
+#define GREEN "\033[;032m"
+
+#define RUN_TEST(test, error_message){\
+    if (test)\
+    {\
+        printf(GREEN);\
+        printf("SUCCESS\n");\
+        printf(NORMAL);\
+    }\
+    else\
+    {\
+        printf(RED);\
+        printf("FAIL %s\n", error_message);\
+        printf(NORMAL);\
+    }\
+}
+
+static int IsBefore(const void *node_data,const void *user_data, void *param)
+{
+    return (*(int*)user_data < *(int*)node_data);
+}
+
+static int MatchFunc(void* node_data, void* user_data)
+{
+    void *n = node_data;
+    
+    return (*(int*)n == *(int*)user_data);
+}
+
+static void SortedListTest1()
+{
+    
+    int data = 77;
+    int data2 = 22;
+    int data3 = 66;
+    int data4 = 99;
+    int ap = 66;
+    
+    sll_iterator_t it1;
+    sll_iterator_t it2;
+    sll_iterator_t it3;
+    sll_iterator_t it4;
+    sll_t *sll1 = NULL;
+    
+    printf("Sorted list test1:\n");
+    
+    sll1 = SortLLCreate(&IsBefore, NULL);
+    RUN_TEST(1 == SLLIsEmpty(sll1), "is empty test1");
+    it1 = SortLLInsert(sll1, &data);
+    RUN_TEST(1 == SLLSize(sll1), "size tes1");
+    RUN_TEST(77 == *(int*)SLLGetData(it1), "get data test1");
+    RUN_TEST(1 == SLLIsSameIter(it1, it1), "same iter test1");
+    
+    SortLLDestroy(sll1);
+    
+    printf("\nSorted list test2:\n");
+    /* test SLLInsert */
+    sll1 = SortLLCreate(&IsBefore, NULL);
+    RUN_TEST(1 == SLLIsEmpty(sll1), "is empty test2");
+    it1 = SortLLInsert(sll1, &data);
+    it2 = SortLLInsert(sll1, &data2);
+    it3 = SortLLInsert(sll1, &data3);
+    RUN_TEST(3 == SLLSize(sll1), "size tes2");
+    RUN_TEST(77 == *(int*)SLLGetData(it1), "get data test2");
+    RUN_TEST(22 == *(int*)SLLGetData(it2), "get data test2");
+    RUN_TEST(66 == *(int*)SLLGetData(it3), "get data test2");
+    
+    SortLLDestroy(sll1);
+    
+    printf("\nSorted list test3:\n");
+    /* test SLLFind */
+    sll1 = SortLLCreate(&IsBefore, NULL);
+    RUN_TEST(1 == SLLIsEmpty(sll1), "is empty test3");
+    it1 = SortLLInsert(sll1, &data);
+    it2 = SortLLInsert(sll1, &data2);
+    it3 = SortLLInsert(sll1, &data3);
+    it4 = SortLLInsert(sll1, &data4);
+    RUN_TEST(4 == SLLSize(sll1), "size tes3");
+    
+    it1 = SLLFind(sll1, &ap, SLLBegin(sll1), SLLEnd(sll1));
+    RUN_TEST(77 == *(int*)SLLGetData(it1), "get data test3");
+    it1 = SLLFindBy(sll1, SLLBegin(sll1), SLLEnd(sll1), &MatchFunc, &ap);
+    RUN_TEST(66 == *(int*)SLLGetData(it1), "get data test3");
+    
+    SortLLDestroy(sll1);
+    printf("\n");
+}
+
+int main()
+{
+    SortedListTest1();
+    return 0;
+}
