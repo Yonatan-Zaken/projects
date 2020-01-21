9e69708e3126c61fbd0e9ba242b4721ac0b74175
yonatan.zaken <yonatan.zaken@gmail.com>
2020-01-21 20:27:22 +0200

    hash

--- ds/hash/hash.c
+++ ds/hash/hash.c
@@ -20,15 +20,37 @@ struct Hash
     dll_t *table[1];
 };
 
+static hash_t *InitTable(hash_t * hash)
+{
+    size_t i = 0;
+    size_t size = hash->table_size;
+    
+    for (i = 0; i < size; ++i)
+    {
+        
+    }
+}
+
 hash_t *HashCreate(size_t table_size, hash_func_t hash_func, action_func_t is_match)
 {
-            
-
+    hash_t *hash = (hash_t*)malloc(offsetof(hash_t, table) + table_size * sizeof(dll_t));
+    if (NULL == hash)
+    {
+        return NULL;
+    }
+    
+    hash->table_size = table_size;
+    hash->is_match = is_match;
+    hash->hash_func = hash_func;
+    
+    InitTable(hash);
+    
+    return hash;
 }
 
 void HashDestroy(hash_t *hash_table)
 {
-
+    
 }
 
 int HashInsert(hash_t *hash_table, void *data)
